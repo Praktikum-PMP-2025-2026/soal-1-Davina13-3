1dfcabe77af3ea65bd753b171c98e0883df6c393
Davina13 <davinaalyashakira@gmail.com>
2026-04-21 09:34:37 +0700

    Refactor soal1.c to use dynamic memory allocation

--- soal1.c
+++ soal1.c
@@ -6,33 +6,39 @@
  *   Deskripsi           : Program untuk memulihkan naskah kuno yang rusak dengan menghapus simbol kurung agar struktur teks kembali valid, tanpa mengubah urutan karakternya.
  */
 
- #include <stdio.h>
- #include <string.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main (){ 
+int main (){
+    char *str = malloc(100 * sizeof(char));
 
-    char str [100];
     printf("");
-    fgets(str, sizeof(str), stdin);
-    str[strcspn(str, "\n")] = '\0';
+    fgets(str, 100, stdin);
+    str[strcspn(str, "\n")] = '\0'; 
 
-    int len = strlen(str);
-    char hasil [100];
-    int i = 0, j = 0, b = 0;
-    for (i = 0; i < len; i++){
-        if (str[i] == '('){
+    int len = strlen(str); 
+
+    char *hasil = malloc(100 * sizeof(char));
+    int i = 0;
+    int b = 0;
+
+    for (int i = 0; i < len; i++) {
+        if (str[i] == '(') {
             b++;
-            hasil[j++] = str[i];
-        } else if (str[i] == ')'){
-            if (b > 0){
+            hasil[i++] = str[i];
+        } else if (str[i] == ')') {
+            if (b > 0) {
                 b--;
-                hasil[j++] = str[i];
+                hasil[i++] = str[i];
             }
         } else {
-            hasil[j++] = str[i];
+            hasil[i++] = str[i];
         }
     }
-    hasil[j] = '\0';
+    hasil[i] = '\0';
+
     printf("%s\n", hasil);
+
     return 0;
 }
