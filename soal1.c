54115a092fa1c26257f4248375be98b18a35f4af
Davina13 <davinaalyashakira@gmail.com>
2026-04-21 10:13:25 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -7,24 +7,18 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-int main (){
+int main(){
     char str[100];
     printf("");
     fgets(str, sizeof(str), stdin);
     str[strcspn(str, "\n")] = 0; 
 
     int len = strlen(str);
-    char *result = (char *)malloc(len + 1); 
-    if (result == NULL) {
-        fprintf(stderr, "Memory allocation failed\n");
-        return 1;
-    }
-
-    int j = 0; // Indeks untuk result
-    int b = 0; // Untuk  keseimbangan tanda kurung
+    char result[100]; 
+    int j = 0; 
+    int b = 0; 
 
     for (int i = 0; i < len; i++) {
         if (str[i] == '(') {
@@ -43,8 +37,5 @@ int main (){
 
     printf("%s\n", result);
     
-    free(result); 
     return 0;
 }
-
-
